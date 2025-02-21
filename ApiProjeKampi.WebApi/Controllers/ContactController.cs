e48c1646a72d7ae5a0c727e628109c0478c5533c
recep <rrecepcakar@gmail.com>
2025-02-21 22:13:55 +0300

    düzenleme

---
+++ ApiProjeKampi.WebApi/Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly ApiContext _context;
+    }
+}

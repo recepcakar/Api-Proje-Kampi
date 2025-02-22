f9d8e11ddce3ba4df4e6c07d8167d82d19caf5b5
recep <rrecepcakar@gmail.com>
2025-02-22 19:59:59 +0300

    Dto oluşturdum

---
+++ ApiProjeKampi.WebApi/Dtos/ContactDtos/CreateContactDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjeKampi.WebApi.Dtos.ContactDtos
+{
+    public class CreateContactDto
+    {
+     
+        public string ContactPhone { get; set; }
+        public string ContactEmail { get; set; }
+        public string MapLocation { get; set; }
+        public string Adress { get; set; }
+        public string OpeningHours { get; set; }
+    }
+}
---
+++ ApiProjeKampi.WebApi/Dtos/ContactDtos/ResultContactDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjeKampi.WebApi.Dtos.ContactDtos
+{
+    public class ResultContactDto
+    {
+        public int ContactId { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactEmail { get; set; }
+        public string MapLocation { get; set; }
+        public string Adress { get; set; }
+        public string OpeningHours { get; set; }
+    }
+}

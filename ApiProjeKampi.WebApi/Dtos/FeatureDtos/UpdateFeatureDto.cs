ff2117a8eefe37bfc20161e5eaddfda062dbe330
recep <rrecepcakar@gmail.com>
2025-02-22 22:21:47 +0300

    bir önceki derste dto ile elle yazdığım kodları bu ders otomatik  yapmak için autıomapper kurdum ve dosyasını oluşturdum aynı zzamanda feature ve message dto su oluşturdum

---
+++ ApiProjeKampi.WebApi/Dtos/FeatureDtos/UpdateFeatureDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjeKampi.WebApi.Dtos.FutureDtos
+{
+    public class UpdateFeatureDto
+    {
+        public int FeatureId { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public string VideoUrl { get; set; }
+        public string İmageUrl { get; set; }
+    }
+}
---
+++ ApiProjeKampi.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,25 @@
+﻿using ApiProjeKampi.WebApi.Dtos.FutureDtos;
+using ApiProjeKampi.WebApi.Dtos.MessageDtos;
+using ApiProjeKampi.WebApi.Entities;
+using AutoMapper;
+
+namespace ApiProjeKampi.WebApi.Mapping
+{
+    public class GeneralMapping  //: Profile //Profile sınıfından mirasa aldık
+    {
+        public GeneralMapping()
+        {
+           /* CreateMap<Feature,CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature,UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature,ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature,GetByIdFeatureDto>().ReverseMap();
+
+
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Message,CreateMessageDto>().ReverseMap();
+            CreateMap<Message,GetByIdMessageDto>().ReverseMap();
+            CreateMap<Message,UpdateMessageDto>().ReverseMap();
+        */
+            }
+    }
+}
